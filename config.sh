d5ca7c7d7f70b717b63c08b846d6343b72e160a2
avsigaev <34997010+avsigaev@users.noreply.github.com>
2018-09-14 23:09:26 +0300

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,11 @@
+tag="rds" #cluster name, bid's and tasks will use it
+numberofnodes="2" # cluster size
+#node config
+ramsize="256" # MB, integers only
+storagesize="1" # GB, integers only
+cpucores="1" #number of cores, integers only
+sysbenchsingle="500"
+sysbenchmulti="600"
+netdownload="10" # Mbits, integers only
+netupload="10" # Mbits, integers only
+price="0.002" # $ per hour
