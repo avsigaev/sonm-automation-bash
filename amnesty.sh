bd76bb1b84ec388d94d47b0c86b3073a78e9ac18
Alexander Sigaev <igoodwin@avsigaev-MBP.sonm.space>
2018-09-24 15:16:27 +0300

    Feat: amnesty

--- amnesty.sh
+++ amnesty.sh
@@ -1,4 +1,27 @@
-for i in $(sonmcli blacklist list --out=json | jq '.addresses' | tr -d '"[],\0') 
-do 
-sonmcli blacklist remove $i && echo $i
-done
+set_sonmcli() {
+	if [ -f "./sonmcli" ]; then
+		sonmcli="./sonmcli"
+	else
+		sonmcli="sonmcli"
+	fi
+}
+
+
+amnesty() {
+	addrs=$($sonmcli blacklist list --out=json | jq '.addresses' | tr -d '"[],\0') 
+
+	if [ $addrs != "null" ]; then
+			echo 'Blaclisted suppliers: ' $addrs
+
+			for i in $addrs
+			do  
+			$sonmcli blacklist remove $i && echo $i
+			done
+
+		else echo 'Blacklist is clean'
+
+	fi
+}
+
+set_sonmcli
+amnesty
